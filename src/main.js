43841127dd156ee218795c1d08c23179330144ca
clm110 <275298711@qq.com>
2017-11-18 16:40:56 +0800

    delete log

--- src/main.js
+++ src/main.js
@@ -4,14 +4,13 @@ import App from './App'
 import router from './router'
 import VeeValidate from 'vee-validate'
 
-// import VueAxios from 'vue-axios'
 Vue.config.productionTip = false
 Vue.use(iView)
 
 Vue.use(VeeValidate)
 var VueCookie = require('vue-cookie')
 Vue.use(VueCookie)
-/* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   router,
